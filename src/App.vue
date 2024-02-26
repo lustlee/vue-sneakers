c9885e86c35557123d0f59d1682dacc16f71b02b
Ramazan Tokoshev <r.tokoshev@gmail.com>
2024-02-26 19:27:30 +0600

    crete component header, and turnOff in Esling vue-multy-component-names

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup>
+import Header from './components/Header/Header.vue'
 </script>
 
 <template>
-  <h1>Aramushi blog!</h1>
+  <div
+    class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <Header />
+
+    <h1>Aramushi blog!</h1>
+  </div>
 </template>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <a href="#" class="inline-block">
+        <img src="/logo.png" alt="Logo" class="w-10">
+      </a>
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Snickers</h2>
+        <p class="text-slate-400">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+        <img src="/cart.svg" alt="Cart">
+        <b>1205 руб.</b>
+      </li>
+      <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+        <img src="/heart.svg" alt="Bookmarks">
+        <span>Закладки</span>
+      </li>
+      <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+        <img src="/profile.svg" alt="Profile">
+        <span>Профиль</span>
+      </li>
+    </ul>
+
+  </header>
+</template>
